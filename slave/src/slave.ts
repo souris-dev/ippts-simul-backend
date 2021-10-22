46ecabf4fd83b0edd368ed43c689163899572301
souris-dev <suorees113@gmail.com>
2021-10-22 11:19:34 +0530

    Added gRPC-node and proto-loader deps

--- slave/src/slave.ts
+++ slave/src/slave.ts
@@ -1,1 +1,17 @@
-import http = require('http');
+import * as grpc from "@grpc/grpc-js"
+import * as protoLoader from "@grpc/proto-loader"
+
+const PROTO_FOLDER: string = "../../proto"
+
+const options: protoLoader.Options = {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+}
+
+const stateInitBcastProto: grpc.GrpcObject = grpc.loadPackageDefinition(
+    protoLoader.loadSync(PROTO_FOLDER + "/state_init_bcase.proto", options)
+);
+
