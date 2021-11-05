e7ff3f4f6baba3f6a5bd6f2c72910cd252704dd2
Soumya Ranjan Tripathy <srtsoumya21@gmail.com>
2021-11-05 19:50:46 +0530

    Added socket_io initialization

--- master/src/master.ts
+++ master/src/master.ts
@@ -3,9 +3,11 @@ import path = require('path');
 import * as http from 'http';
 import * as cors from "cors";
 import { AddressInfo } from 'net';
+import { Server } from 'socket.io';
 
 const app: express.Application = express();
 const port: number = parseInt(process.env.PORT || '5030', 10);
+const port_io: number = parseInt(process.env.PORT_IO || '5031', 10);
 
 var ipptsrouter = require('./routes/ippts');
 
@@ -18,6 +20,27 @@ app.use(cors());
 app.use('/ippts', ipptsrouter);
 
 var server: http.Server = http.createServer(app);
+var server_io: http.Server = http.createServer();
+
+const io = new Server(server_io,{
+  path: '/',
+  pingTimeout: 60000,
+  pingInterval: 25000,
+  transports: ['websocket', 'polling']
+});
+
+io
+.of('/sim')
+.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server_io.listen(port_io, () => {
+  console.log('Socket listening on port:' + port_io);
+});
 
 server.listen(port, () => {
     console.log('Master server listening on port ' + port);
@@ -28,5 +51,4 @@ server.on('listening', onListening);
 function onListening() {
   var addr: string | AddressInfo = server.address();
   var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-
 }
